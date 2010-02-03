e12004dfc84969ba8e92d79bab1a364d1e4cf00d
Alex Barth <ab@a.local>
2010-02-02 21:40:37 -0500

    Security work. Nearly there.

--- sites/all/modules/feeds/feeds_ui/feeds_ui.js
+++ sites/all/modules/feeds/feeds_ui/feeds_ui.js
@@ -87,4 +87,20 @@ Drupal.behaviors.feeds = function() {
     $('#' + $(this).attr('id')).attr('checked', 1);
     $('input.form-submit.feeds-ui-hidden-submit').click();
   });
+
+  // Show keyauth boxes only when checked.
+  if ($('#edit-use-keyauth').attr('checked')) {
+    $('#edit-keyauth-public-wrapper').show();
+  }
+  else {
+    $('#edit-keyauth-public-wrapper').hide();
+  }
+  $('#edit-use-keyauth').click(function() {
+    if ($(this).attr('checked')) {
+      $('#edit-keyauth-public-wrapper').show(200);
+    }
+    else {
+      $('#edit-keyauth-public-wrapper').hide(200);
+    }
+  });
 };
